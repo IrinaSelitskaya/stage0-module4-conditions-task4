fc724cc6828f2ef713338dfb7ef608c70712da00
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-02-09 17:19:59 +0300

    Update TernaryGreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,8 +2,6 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-     first=1;
-     second=12;
      int result=first>=second? first: second;
      System.out.println(result);
 }
